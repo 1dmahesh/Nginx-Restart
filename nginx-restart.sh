6b3ecec2fb517d155b676351f965007f02a4ca15
1dmahesh <69144750+1dmahesh@users.noreply.github.com>
2022-03-09 12:25:30 +0530

    Minor update in nginx-restart.sh

--- nginx-restart.sh
+++ nginx-restart.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+#This below command will get active status from nginx status and save it into a variable called NGINX.
 
- NGINX=$( service nginx status | grep -wn 'active' | cut -d ':' -f2 )
- sleep 2s
+NGINX=$( service nginx status | grep -wn 'active' | cut -d ':' -f3 | awk '{ print $1 }' )
 
+#Now we will run a if statement to check whether Nginx running or not. 
  if [ $NGINX = "Active" ];
-      then
-         echo 'Nginx is working fine'
-         exit 1
+ then
+      echo 'Nginx is working fine'
+      exit 1
  else
        service nginx restart
  echo 'We have restarted the Nginx Processs'
